e901615c6c2b86f6f38875a6c616004f713bb857
jaisotorro <jaisotorro@gmail.com>
2020-04-29 13:31:41 +0200

    Segunda version, despues de hacer primer commit

--- src/logica/GestionColeciones.java
+++ src/logica/GestionColeciones.java
@@ -10,7 +10,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 public class GestionColeciones {
-
+// Comentario de prueba
 	List<String> listNombres;
 	Set<String> conjProductos;
 	Map<Integer,String> mapTareas;
--- src/vista/Menu.java
+++ src/vista/Menu.java
@@ -178,5 +178,5 @@ public class Menu {
 //		System.out.println("Lista de colecciones:");
 //		gesPel.listaColecciones();
 	}
-
+// prueba de otro cambio
 }
